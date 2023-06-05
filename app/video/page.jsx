2b54436cbeb1b0219d7a5a53f10ae8c96f097e26
benamager <benaprivat@gmail.com>
2023-06-06 00:01:13 +0200

    Further improvement to error at video page

--- app/video/page.jsx
+++ app/video/page.jsx
@@ -9,22 +9,28 @@ export default async function Video({ params, searchParams }) {
     throw new Error("No valid YouTube URL found...");
   }
 
-  // fetch the video data via API routes
-  const response = await fetch(`${process.env.URL}/api/video_data?url=${videoUrl}`, { cache: "no-cache" });
+  try {
+    // fetch the video data via API routes
+    const response = await fetch(`${process.env.URL}/api/video_data?url=${videoUrl}`, { cache: "no-cache" });
 
-  // error handling
-  if (response.status !== 200) {
-    const json = await response.json();
-    if (json?.error) {
-      throw new Error(json?.error);
-    } else {
-      throw new Error("Internal server error...");
+    // error handling
+    if (response.status !== 200) {
+      const json = response ? await response.json() : null;
+      const errorMessage = json?.error || "Internal server error...";
+
+      throw new Error(errorMessage);
     }
-  }
 
-  // parse data
-  const json = await response.json();
-  const { transscript, videoDetails } = json;
+    // parse data
+    const json = await response.json();
+    var { transscript, videoDetails } = json;
+  } catch (error) {
+    console.error(error);
+    if (error?.message) {
+      throw new Error(error.message);
+    }
+    throw new Error("Internal server error");
+  }
 
   return (
     <main className="w-full">
